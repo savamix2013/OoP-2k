4f412c3f8cc25683b00847eac1501ab6f2af90b3
Vsevolod Parashchyn <144225141+savamix2013@users.noreply.github.com>
2025-02-26 10:46:27 +0200

    Update class_work3.py

--- class_work3.py
+++ class_work3.py
@@ -4,34 +4,25 @@ class Car:
     """змоделюємо машину"""
 
 
-    def __init__(self, brand_or_car="Unknown", model="Unknown", year=0, mileage=0):
-        """Опис машини який копіює в інший об'єкт Car"""
+    def __init__(self, brand="Unknown", model="Unknown", year=0, mileage=0):
+        """Опис машини"""
         current_year = datetime.datetime.now().year
-
-        if isinstance(brand_or_car, Car):
-            self.brand = brand_or_car.brand
-            self.model = brand_or_car.model
-            self.year = brand_or_car.year
-            self.mileage = brand_or_car.mileage
-        else: # Якщо передали звичайні значення
-            self.brand = brand_or_car
-            self.model = model
-            if year > current_year:
-                print(f"помилка: : рік випуску {year} не може бути більшим за {current_year}. Ставимо {current_year}.")
-                self.year = current_year
-            else:
-                self.year = year
-            self.mileage = mileage
-
+        self.brand = brand
+        self.model = model
+        self.year = year if year <= current_year else current_year
+        self.mileage = mileage
 
     def __del__(self):
         """Видалення"""
-        print("об'єкт знищено")
-
+        print(f"Автомобіль {self.brand} {self.model} видалено з пам'яті.")
 
     def get_info(self):
         """Виводить інформацію про машину""" 
         return f"Марка: {self.brand}, модель: {self.model}, рік: {self.year}, пробіг: {self.mileage}."
+
+    def copy(self):
+        """Створює копію об'єкта"""
+        return Car(self.brand, self.model, self.year, self.mileage)
 
 
     def update_mileage(self, new_mileage):
@@ -59,3 +50,4 @@ class Car:
 
 car1 = Car("Toyota", "Camry", 2030, 50000)  # Тут має виправитися рік
 print(car1.get_info()) 
+
